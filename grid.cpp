3833d790018c7cb4434a82bf562f1972601079e7
Nicknakin <Kinneyanick@gmail.com>
2018-09-12 16:12:57 -0500

    Updated grid and cell based on usage needs

--- grid.cpp
+++ grid.cpp
@@ -1,13 +1,26 @@
 #include "grid.h"
+#include <algorithm>
 
 Grid::Grid(int width, int height, int side, sf::Color defaultColor): width{width}, height{height}, side{side}, defaultColor{defaultColor} {
     for(int i = 0; i < width*height; i++){
-        cells.push_back(Cell{i%width, i/width, side, side, defaultColor.r, defaultColor.g, defaultColor.b});
+        cells.push_back(Cell{i%width, i/width, side, side, (float) defaultColor.r, (float) defaultColor.g, (float) defaultColor.b});
     }
 }
 
+Cell& Grid::getChangedCell(int i){
+    return cells[changedCells[i]];
+}
+
+void Grid::popChangedCell(){
+    changedCells.erase(changedCells.begin(), changedCells.begin()+1);
+}
+
+int Grid::getChangedCellSize(){
+    return changedCells.size();
+}
+
 int Grid::size(){
-    return height;
+    return height*width;
 }
 
 int Grid::getHeight(){
@@ -19,7 +32,7 @@ int Grid::getWidth(){
 }
 
 Cell& Grid::getCell(int x, int y){
-    return getCell(x+y*width);
+    return cells[x+y*width];
 }
 
 Cell& Grid::getCell(int oneDIndex){
@@ -38,7 +51,17 @@ void Grid::setColor(int x, int y, int r, int g, int b){
     cells[x+width*y].setFillColor(sf::Color{r,g,b});
 }
 
+void Grid::setColor(int x, int y, sf::Color color){
+    cells[x+width*y].setFillColor(color);
+}
+
 std::vector<Cell> Grid::operator[](int i){
     return std::vector<Cell>{cells.begin()+i*width, cells.begin()+(i+1)*width};
+}
+
+void Grid::change(int x, int y){
+    if(!changedCells.empty() || !std::count(changedCells.begin(), changedCells.end(), x+y*width)){
+        changedCells.push_back(x+y*width);
+    }
 }
 
--- grid.h
+++ grid.h
@@ -11,20 +11,24 @@ class Grid{
         Grid(int width, int height, int side = 1, sf::Color defaultColor = sf::Color(0,0,0));
         Cell& getCell(int x, int y);
         Cell& getCell(int oneDIndex);
+        Cell& getChangedCell(int onDIndex);
+        void popChangedCell();
+        int getChangedCellSize();
         int size();
         int getWidth();
         int getHeight();
         sf::Color getDefaultColor();
-        std::vector<Cell>& getCells();
-        void setColor(int x, int y, int r, int g, int b);
         std::vector<Cell> operator[](int i);
-        
+        std::vector<Cell>& getCells();
+        void setColor(int x, int y, sf::Color color);
+        void setColor(int x, int y, int r, int g, int b);
+        void change(int x, int y);
 
     private:
         int width, height, side;
         const sf::Color defaultColor;
         std::vector<Cell> cells;
-        std::vector<Cell> changedCells;
+        std::vector<int> changedCells;
 };
 
 #endif
